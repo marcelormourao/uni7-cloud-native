d749b7b0c3528651451a7b42e01514dc3f6e9473
Marcelo Mourão <marcelo.rmourao@gmail.com>
2020-08-22 11:52:43 -0300

    v1.0.1 - Adding comunication with other microservice

--- src/main/java/mourao/ribeiro/marcelo/demo/controller/DemoController.java
+++ src/main/java/mourao/ribeiro/marcelo/demo/controller/DemoController.java
@@ -1,16 +1,48 @@
 package mourao.ribeiro.marcelo.demo.controller;
 
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class DemoController {
+	
+	@Value("${microservices.other:http://localhost:8080/demo/sum}")
+	private String other;
+	
+	private final static Logger logger = LoggerFactory.getLogger(DemoController.class);
 
+	@GetMapping(value="other", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Integer> other() {
+		RestTemplate rest = new RestTemplate();
+		
+		final int min = 1;
+		
+		final int max = 20;
+		
+		Integer a = min + new Random().nextInt(max - min + 1);
+		
+		Integer b = new Random().nextInt(2) + 5;
+		
+		logger.info("Calling " + this.other+"?a="+a+"&b="+b);
+		
+		String body = rest.getForEntity(this.other+"?a="+a+"&b="+b, String.class).getBody();
+		
+		return ResponseEntity.ok(Integer.parseInt(body));
+	}
+	
 	@GetMapping(value="sum", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Integer> sum(@RequestParam(required = true) Integer a, @RequestParam(required = true) Integer b) {
+		logger.info("Summing " + a + " + " + b);
+		
 		return ResponseEntity.ok(a + b);
 	}
 }
